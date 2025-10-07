8522a9ac92a00477860d94ace7c32a5459f6f5fe
rafaelvenetikides <rafavenetikides@gmail.com>
2025-10-07 10:08:02 -0300

    Initial Commit

---
+++ payment-tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  payment-tracker
+//
+//  Created by Rafael Venetikides on 07/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
